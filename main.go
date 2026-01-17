94e0393121f201343a929ac8320702d63d43e5d2
Ocidemus <codeofascendence@gmail.com>
2026-01-17 10:56:54 +0000

    C: add metrics sys

--- main.go
+++ main.go
@@ -1,16 +1,52 @@
 package main
 
 import (
-	"net/http"
+	"fmt"
 	"log"
+	"net/http"
+	"sync/atomic"
 )
+
+func endpoint(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK) // 200
+	w.Write([]byte("OK"))
+
+}
+
+func(cfg *apiConfig) metrics(w http.ResponseWriter,r *http.Request){
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK) // 200
+	hits := cfg.fileserverHits.Load()
+	w.Write([]byte(fmt.Sprintf("Hits: %d",hits)))
+}
+
+func(cfg *apiConfig) reset(w http.ResponseWriter,r *http.Request){
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK) // 200
+	cfg.fileserverHits.Store(0)
+}
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter,r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w , r)
+	})
+}
 
 func main(){
 	const filepathRoot = "."
 	const port = "8080"
-
+	cfg := &apiConfig{}
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
+	mux.HandleFunc("/healthz", endpoint)
+	mux.HandleFunc("/metrics", cfg.metrics)
+	mux.HandleFunc("/reset", cfg.reset)
+	mux.Handle("/app/", cfg.middlewareMetricsInc(http.StripPrefix("/app",http.FileServer(http.Dir(filepathRoot)))))
 
 	srv := &http.Server{
 		Addr:    ":" + port,
